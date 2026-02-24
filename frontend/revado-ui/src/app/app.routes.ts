26fff16f95b1d2b0d6127d41c55fe0cf3de5f703
Fiqre-Ab <fiqreyesusabraham@gmail.com>
2026-02-23 18:18:38 -0800

    add login and register

--- frontend/revado-ui/src/app/app.routes.ts
+++ frontend/revado-ui/src/app/app.routes.ts
@@ -1,7 +1,15 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
+import { Login } from './pages/components/login/login';
+import { Register } from './pages/components/register/register';
 
 
 export const routes: Routes = [
+      {path:'login', component: Login},   
+      {path:'register', component: Register},
+      
       { path: 'home', component: Home   },
+      {
+            path:'',redirectTo:'home', pathMatch:'full'
+      }
 ];
